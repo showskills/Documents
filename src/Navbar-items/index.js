aa64bf04d28568968748a45a7b452c638fa256d6
AmanLohan <70275102+AmanLohan@users.noreply.github.com>
2021-04-12 14:03:35 +0530

    delete src public

--- src/Navbar-items/index.js
+++
@@ -1,5 +0,0 @@
-export {default as Lists} from './Lists'
-export {default as Messages} from './Messages';
-export {default as Orders} from './Orders';
-export {default as Profile} from './Profile';
-export {default as SignIn} from './SignIn';
--- src/components/index.js
+++
@@ -1,3 +0,0 @@
-export {default as FlCard} from './FlCard';
-export {default as NavBar} from './NavBar';
-export {default as SendMessageLayout} from './SendMessageLayout';
