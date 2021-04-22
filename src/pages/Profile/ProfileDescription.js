39ea513e5cc2709f11e73b4e41e6dbfd78aa931a
rohitbajaj7877 <54737825+rohitbajaj7877@users.noreply.github.com>
2021-04-22 13:33:20 +0530

    modified profile description page

--- src/pages/Profile/ProfileDescription.js
+++ src/pages/Profile/ProfileDescription.js
@@ -1,105 +1,146 @@
 import {db} from '../../lib/firebase.prod';
 import React, { useEffect, useState } from 'react';
-import {Container,Row ,Col,Button} from 'react-bootstrap';
+import {Carousel,Container,Row ,Col,Button} from 'react-bootstrap';
 import './profilePage.css';
-import { set } from 'react-hook-form';
+import { Link } from "react-router-dom";
 
 
 const ProfileDescription=()=>{
+      const [profiledata,setprofileData]=useState({});
+      const [gigdata,setgigData]=useState({});
+      const [skills,setSkills]=useState([]);
 
-   const [GigURL,setGigURL]=useState("");
-   const [UserURL,setUserURL]=useState("");
-   
-
-
-   const ProfileData=()=>{
-   const uid="WTedB4smDdT22lSgV1yW1tzSQpu1";
+      const profiledb=()=>{
+         const uid="WTedB4smDdT22lSgV1yW1tzSQpu1";
      db.collection('freelancer-profile').doc(uid).get().
      then(doc=>{
-        const data=doc.data();
-
-        setUserURL(data['ProfilePhotoUrl']);
-        document.getElementById('name').innerHTML=data['Username'];
-        document.getElementById('des').innerHTML=data['Description'];
-        document.getElementById('skills').innerHTML="Professional in: <br>";
-        data['Skills'].map(skill=>{
-         document.getElementById('skills').innerHTML+="\xa0# ";
-         document.getElementById('skills').innerHTML+=skill.skillName;
-         document.getElementById('skills').innerHTML+="<br>";
+        var array=[]; 
+        document.getElementById('print').innerHTML="";
+        doc.data()['Skills'].map(skill=>{
+        array.push(skill.skillName);
+        document.getElementById('print').innerHTML+='<br/># ';    
+        document.getElementById('print').innerHTML+=skill.skillName;
         })
-              
+        setSkills(array);
+        console.log(array)
+         setprofileData(doc.data());                   
      })   
-   }
+      }
 
-   const GigData=()=>{
-      
+      const gigdb=()=>{
          const uid="WTedB4smDdT22lSgV1yW1tzSQpu1";
+     db.collection('Gig-Data').doc(uid).get().
+     then(doc=>{
+         setgigData(doc.data());                   
+     })   
+      }
+
+      useEffect(()=>{
+         gigdb();
+      },[]);
    
-         db.collection('Gig-Data').doc(uid).get().then(doc=>{
-              const data=doc.data();
-              document.getElementById('instructions').innerHTML=" Message to the Users:<br>"+data['Instructions']
-            document.getElementById('gigTitle').innerHTML=data['Title'];
-            document.getElementById('gigDes').innerHTML=data['Description'];
-            document.getElementById('gigCategory').innerHTML=data['Category']+" projects";
-            document.getElementById('gigSubcategory').innerHTML= "\xa0 prefably on "+data['SubCategory'];
-            document.getElementById('gigPrice').innerHTML=" Starting from Rs."+ data['Price'];
-            document.getElementById('gigDuration').innerHTML=data['Duration']+" delivery";          
-             setGigURL(data['PhotoURL']);
-          })
-   }
-   useEffect(()=>{
-      GigData();
-   },[]);
-
-   useEffect(()=>{
-      ProfileData();
-   },[]);
-
+      useEffect(()=>{
+         profiledb();
+      },[]);
    
-
+      
 
 
    return ( <>
-   <div >
-   <Container>
-     <Row>
-        <Col>
-        <h2 id="gigTitle" ></h2>
-         <img class="gigPic" src={GigURL} />
-        <h1>About the gig</h1>
-     
-       <h id="gigDes" ></h><br></br><br></br>
-       <h>Like to work on </h>
-       <p id="gigCategory"></p>
-        <p id="gigSubcategory"></p><br></br>
-       <p id="gigPrice"></p> 
-       <p id="gigDuration"></p> <br></br>
-       <p id='instructions'></p>
-     
-   </Col>
-   </Row>
-   </Container>
-    </div>
 
-   <div>
-    <Container>
-  <Row>
-    <Col>
-       <h1>About The Seller</h1>
-       <img id="profilePic" src={UserURL}/><br></br>
-       <div id="user">
-       <p id="name"></p>
-       <p id="des" ></p>
-       <p id="skills"></p>   
-       </div> 
-       
-    </Col>
-  </Row>
-</Container>
-    </div>
+             <div >
+             <Container>
+                <Row>
+                   <Col>
+
+                   <div>
+                     
+                    <h className="heading12">{gigdata['Title']}</h><br/>
+                    <Button className="confirmOrder">Continue &#8377; ({gigdata['Price']})</Button>
+                  
+                     <span><img class="pic11" src={profiledata['ProfilePhotoUrl']}/></span>
+                     <span>{profiledata['Username']}</span>&nbsp;
+                     <span className="staryell">{4.9}&#x2605;&#x2605;&#x2605;&#x2605;&#x2605;</span>
+                    </div>
+                     <br/>
+                     
+                   </Col>
+                </Row>
+
+                <Row>
+                   <Col>
+                   <img className="gigpic34" src={gigdata['PhotoURL']}/><br/>
+
+                   <h1>About the Gig</h1>
+                   <p>{gigdata['Description']}</p><br></br>
+                   <p> I like to work on {gigdata['Category']}&nbsp; projects</p>
+                   <p>&nbsp;&#10004; Expertise in {gigdata['SubCategory']}</p>
+                   <p>Starting from Rs. {gigdata['Price']}</p>
+                   <p>{gigdata['Duration']} Delivery</p><br></br>
+                   <p>Instructions for the User:<br/> {gigdata['Instructions']}</p>              
+                   </Col>
+                </Row>
+                  <br/><br/>
+
+                <Row>
+                   <Col>
+                      <h1>About the Seller </h1>
+
+                      <div className="box12">
+                      <img className="userpic2" src={profiledata['ProfilePhotoUrl']}/>
+
+                      <div className="box123">
+                        <p>{profiledata['Username']}</p>
+                        <p>{profiledata['Description']}</p>
+                        <p className="staryell">{4.9}&#x2605;&#x2605;&#x2605;&#x2605;&#x2605;</p>
+                        <Button >Contact me</Button>
+                       </div>
+                     </div><br/>
+                     
+                   <div className="box1234">
+                   <h1>Proffesional in :</h1>
+                   <p id="print"></p>
+                   {/* <p>{ 
+                       skills.map(skill=>{                    
+                        <p>{skill}</p>
+                       })
+                    }
+                  </p>*/}
+                  
+                  </div>
+                   </Col>
+                </Row>
+             </Container>
+    
 
 
+             </div>
+             <h className="heading123">What people loved about this Seller</h><br/><br/>
+            
+             <Carousel>
 
+  <Carousel.Item interval={5000}>
+  <p>Absolutely Incredible!! The best value, best communication! First time using Fiverr and investing in my side business! BEST THING MONEY COULD BUY!!!</p>
+  <Link className="link34" to="/">more...</Link>
+    <img
+      className="d-block w-100"
+      src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAT4AAACfCAMAAABX0UX9AAAAA1BMVEUAAACnej3aAAAASElEQVR4nO3BMQEAAADCoPVPbQ0PoAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABODcYhAAEl463hAAAAAElFTkSuQmCC"
+      alt="First slide"
+    />  
+    
+  </Carousel.Item>
+  <Carousel.Item interval={5000}>
+  <p>I don‘t have anything to say thats negative. He did his work exactly like I described it to him, he was very kind, answered my questions if I had some, answered and delivered in hours. I would recommend him to everybody who is thinking about ordering a logo. His work is amazing</p>
+  <Link className="link34" to="/">more...</Link>
+    <img
+      className="d-block w-100"
+      src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAT4AAACfCAMAAABX0UX9AAAAA1BMVEUAAACnej3aAAAASElEQVR4nO3BMQEAAADCoPVPbQ0PoAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABODcYhAAEl463hAAAAAElFTkSuQmCC"
+      alt="First slide"
+    />  
+   
+  </Carousel.Item>
+</Carousel>
+<br></br>
            </>
    );
 };
