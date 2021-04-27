3baa9273db2179cff7c8a43447c7b592a1553cb4
rohitbajaj7877 <54737825+rohitbajaj7877@users.noreply.github.com>
2021-04-27 17:26:20 +0530

    display project page

--- src/pages/UserProjects/DataHandeling.js
+++ src/pages/UserProjects/DataHandeling.js
@@ -41,20 +41,46 @@ const ActiveProjectsDataUpdate = async (props)=>{
 }
 
 const ActiveProjectsDataRetrieve=async (props)=>{
+
       const {userid} = props;
     var FreelancingProjects=[],RecruitingProjects=[];
+    var FpNames=[],RpNames=[];
   var ref =db.collection('ActiveProjects').doc(userid);
+
    await ref.get().then(doc=>{
-
+        
        if((doc.exists))
-       {FreelancingProjects=doc.data().Freelancing;
-    RecruitingProjects=doc.data().Recruiting;}
-
+       {FreelancingProjects=doc.data().FreelancingProjects;
+    RecruitingProjects=doc.data().RecruitingProjects;}
+  
    });
-
-   return {FreelancingProjects,RecruitingProjects}
+      FpNames= await ProjectNames(FreelancingProjects);
+      RpNames=await ProjectNames(RecruitingProjects);
+    
+   console.log(FreelancingProjects)
+   return {FreelancingProjects,RecruitingProjects,FpNames,RpNames};
 
 }
+
+const ProjectNames=async (projects)=>{
+    
+  var pnames=[];
+  projects.map(async (project,i)=>{
+
+     var ref=db.collection('Projects').doc(project.ProjectId);
+     await ref.get().then(
+         doc=>
+         { pnames.push(doc.data().projectName);
+               
+         }
+     )
+  });
+  console.log(pnames)
+   return pnames;
+
+}
+
+
 
 
 export {ActiveProjectsDataRetrieve,ActiveProjectsDataUpdate};
--- src/pages/UserProjects/Projects.js
+++ src/pages/UserProjects/Projects.js
@@ -1,39 +1,125 @@
 import {ActiveProjectsDataRetrieve} from './DataHandeling';
 import useAuthListener from "../../hooks/use-auth-listener";
 import { useEffect, useState } from 'react';
+import './Projects.css';
 
 
 const Projects=()=>{
       
 
     const currentUser=useAuthListener().user;
-    const [freelancingProjects,setfProjects]=useState([]);
-    const [recruitingProjects,setrProjects]=useState([]);
+    const [freelancingProjects,setfp]=useState([]);
+    const [recruitingProjects,setrp]=useState([]);
+    const [numerOfFProjects,setfpNumber]=useState(0);
+    const [numerOfRProjects,setrpNumber]=useState(0);
+    const [FreelancingprojectNames,setfprojectNames]=useState([]);
+    const [RecruitingprojectNames,setrprojectNames]=useState([]);
 
 
     const getProjectsData=async ()=>{
 
-    var  {FreelancingProjects,RecruitingProjects}=
-    await ActiveProjectsDataRetrieve(currentUser.uid);
-    setfProjects(FreelancingProjects);
-    setrProjects(RecruitingProjects);
+    var  {FreelancingProjects,RecruitingProjects,FpNames,RpNames}=
+    await ActiveProjectsDataRetrieve({userid:currentUser.uid});
+
+    console.log(FreelancingProjects);
+    console.log(RecruitingProjects);
     
+    
+    setfp(FreelancingProjects);
+    setrp(RecruitingProjects);
+
+    setfpNumber(FreelancingProjects.length);
+    setrpNumber(RecruitingProjects.length);
+
+    setfprojectNames(FpNames);
+    setrprojectNames(RpNames);
+
 
  };
 
+ 
+
+ const setStatus=({status,projectid,type})=>{ 
+
+
+ }
+ 
+   
 
  useEffect(async ()=>await getProjectsData(),[]);
-
+   console.log(freelancingProjects)
+   console.log(FreelancingprojectNames);
+   console.log(RecruitingprojectNames);
 
     return (
         <>
+
         <div>
+            <h3 className="heading34">
         Freelancing Projects:
+            </h3>
+        {
+
+        freelancingProjects !== [] ? 
+        
+        [...Array(numerOfFProjects)].map((e,i)=>
+        <h clasName="content12" >
+          
+           <h className="project-info">
+           
+            <span>{i+1}.</span>
+            <span>{FreelancingprojectNames[i]}</span>
+            <button id="showstatus" disabled>{freelancingProjects[i].ProjectStatus}</button>
+
+            <select className="gigDropdown" id="projectstatus" name="projectstatus" 
+            >
+                  
+                <option value="Active" selected >Active</option>
+                <option value="Completed">Completed</option>
+                <option value="Abandoned">Abandoned</option>         
+            </select>
+              <br />
+           </h>
+           </h>
+        )
+
+        : ''
+
+        }
         </div>
+
+
         <div>
-        Recruiting Projects:
-        </div>
-             
+        <h3 className="heading34">
+        Recruiting  Projects:
+            </h3>
+        {
+
+        recruitingProjects !== [] ? 
+ 
+        [...Array(numerOfRProjects)].map((e,i)=>
+        <h clasName="content12 " >
+  
+             <h className="project-info">
+         <span>{i+1}.</span>
+        <span>{RecruitingprojectNames[i]}</span>
+        <button id="showstatus" disabled>{recruitingProjects[i].ProjectStatus}</button>
+
+         <select className="gigDropdown" id="projectstatus" name="projectstatus" 
+            >
+          
+                <option value="Active" selected >Active</option>
+                <option value="Completed">Completed</option>
+                <option value="Abandoned">Abandoned</option>         
+         </select>
+             <br />
+             </h>
+             </h>
+              )
+
+         : ''
+        }
+        </div>          
         </>
 
     )
