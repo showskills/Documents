5c9efdd7444ca8aaa0dad6f9b183b91eeb7a45b5
Raman Ailawadhi <79252024+ultimate-raman@users.noreply.github.com>
2021-04-30 21:37:15 +0530

    Final touch

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,55 +1,58 @@
 import React from "react";
 import "./Footer.css";
 import { Link } from "react-router-dom";
+import { Navbar,Nav} from "react-bootstrap";
 
 function Footer() {
   const currentYear = new Date().getFullYear();
   return (
       <>
     <hr/>
-        <div className="main">
-          <div>
+    <div style={{backgroundColor: "white", border: "1px solid grey", margin: "50px 0px", padding: "5px"}}>
+    <Navbar bg="light" expand="md">
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+    <Nav>
+
           <dl>
             <dt>Categories</dt>
-              <Link to='/'><dd>Web Development</dd></Link>
-              <Link to='/'><dd>Mobile Development</dd></Link>
-              <Link to='/'><dd>Data Science</dd></Link>
-              <Link to='/'><dd>Operating System</dd></Link>
-              <Link to='/'><dd>Blockchain</dd></Link>
-              <Link to='/'><dd>Design Tools</dd></Link>
-              <Link to='/'><dd>Database Design & Development</dd></Link>
+              <Nav.Link className="mr-lg-3 mr-md-2"  as={Link} to="/">Web Development</Nav.Link>
+              <Nav.Link className="mr-lg-3 mr-md-2"  as={Link} to="/">Mobile Development</Nav.Link>
+              <Nav.Link className="mr-lg-3 mr-md-2"  as={Link} to="/">Data Science</Nav.Link>
+              <Nav.Link className="mr-lg-3 mr-md-2"  as={Link} to="/">Operating System</Nav.Link>
+              <Nav.Link className="mr-lg-3 mr-md-2"  as={Link} to="/">Blockchain</Nav.Link>
+              <Nav.Link className="mr-lg-3 mr-md-2"  as={Link} to="/">Design Tools</Nav.Link>
+              <Nav.Link className="mr-lg-3 mr-md-2"  as={Link} to="/">Database Design & Development</Nav.Link>
           </dl>
-        </div>
-  <div>
+
         <dl>
             <dt>About</dt>
-              <Link to='/privacypolicy'><dd>Privacy Policy</dd></Link>
-              <Link to='/termsofservice'><dd>Terms of Service</dd></Link>
-              <Link to='/footerabout'><dd>About US</dd></Link>
+            <Nav.Link className="mr-lg-3 mr-md-2"  as={Link} to="/privacypolicy">Privacy Policy</Nav.Link>
+            <Nav.Link className="mr-lg-3 mr-md-2"  as={Link} to="/termsofservice">Terms of Service</Nav.Link>
+            <Nav.Link className="mr-lg-3 mr-md-2"  as={Link} to="/footerabout">About US</Nav.Link>
         </dl>
-  </div>  
-  <div>
         <dl>
             <dt>Support</dt>
-              <Link to='/contactus'><dd>Contact Us</dd></Link>
-              <Link to='/freelancerfaq'><dd>Freelancer FAQ</dd></Link>
-              <Link to='/recruiterfaq'><dd>Recruiter FAQ</dd></Link>
+            <Nav.Link className="mr-lg-3 mr-md-2"  as={Link} to="/contactus">Contact Us</Nav.Link>
+            <Nav.Link className="mr-lg-3 mr-md-2"  as={Link} to="/freelancerfaq">Freelancer FAQ</Nav.Link>
+            <Nav.Link className="mr-lg-3 mr-md-2"  as={Link} to="/recruiterfaq">Recruiter FAQ</Nav.Link>
         </dl>
-  </div>  
-  <div>
+
         <dl>
             <dt>More From ShowSkills</dt>
-              <Link to='/startselling'><dd>Selling on ShowSkills</dd></Link>
-              <Link to='/'><dd>Buying on ShowSkills</dd></Link>
-              <Link to='/startselling'><dd>Become a Seller</dd></Link>
+            <Nav.Link className="mr-lg-3 mr-md-2"  as={Link} to="/startselling">Selling on ShowSkills</Nav.Link>
+            <Nav.Link className="mr-lg-3 mr-md-2"  as={Link} to="/">Buying on ShowSkills</Nav.Link>
+            <Nav.Link className="mr-lg-3 mr-md-2"  as={Link} to="/startselling">Become a Seller</Nav.Link>
         </dl>
-  </div>  
-  <div>
-        <dl>
-            <dt>Community</dt>
-              <Link to='/blog'><dd>Blog</dd></Link>
-        </dl>
-  </div>
+
+    <dl>
+        <dt>Community</dt>
+        <Nav.Link className="mr-lg-3 mr-md-2"  as={Link} to="/blog">Blog</Nav.Link>
+    </dl>
+    
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
 </div>
     <footer>
     <hr/>
